28c2802a6e9fcf1e3b522d20537566b8aa40363d
Andrei Kukhto <andrei060656@mail.ru>
2021-02-05 20:53:23 +0300

    create product page and add tests for product page and add locatars for product

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_formm")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_formm")
+
+
+class ProductPageLocators:
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "h1")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, ".price_color")
+    ALERT_ADDING_PRODUCT = (By.CSS_SELECTOR, "#messages .alertinner strong")
+    ALERT_PRICE_BASKET = (By.CSS_SELECTOR, "#messages .alertinner p strong ")
---
+++ pages/product_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        add_basket_button.click()
+
+    def should_be_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_BUTTON), "Basket button is not presented"
+
+    def should_be_alert_about_adding(self):
+        assert self.is_element_present(*ProductPageLocators.ALERT_ADDING_PRODUCT), "Alert about adding is not presented"
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product name is not presented"
+        alert_text = self.browser.find_element(*ProductPageLocators.ALERT_ADDING_PRODUCT).text
+        name_product = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        assert alert_text == name_product, "Product name is not right"
+
+    def should_be_alert_with_price_basket(self):
+        assert self.is_element_present(*ProductPageLocators.ALERT_PRICE_BASKET), "Alert with price is not presented"
+        assert self.is_element_present(*ProductPageLocators.PRICE_PRODUCT), "Product price is not presented"
+        alert_price = self.browser.find_element(*ProductPageLocators.ALERT_PRICE_BASKET).text
+        price_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text.split(" ")[0]
+        assert price_product in alert_price, "Price is not right"
